c777ebbe4290867b8cfc3caaa78e8e8969321ccd
gumelarid <galuhprahadi96@gmail.com>
2022-11-02 09:14:14 +0700

    update

--- app/Http/Controllers/cms/NavigationController.php
+++ app/Http/Controllers/cms/NavigationController.php
@@ -3,17 +3,25 @@
 namespace App\Http\Controllers\cms;
 
 use App\Models\navigation;
-use Illuminate\Support\Str;
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Requests\NavigationRequest;
-use Illuminate\Http\Request;
+use App\Repository\Navigation\NavigationImplement;
 
 class NavigationController extends Controller
 {
+
+    protected $navigationImplement;
+
+    public function __construct(NavigationImplement $navigationImplement)
+    {
+        $this->navigationImplement = $navigationImplement;
+    }
+
     public function index()
     {
         $title = "List Navigation";
-        $data = navigation::orderBy('id_label', 'asc')->get();
+        $data = $this->navigationImplement->getAll();
         return \view('cms.pages.navigation.index', \compact('title', 'data'));
     }
 
@@ -21,14 +29,7 @@ class NavigationController extends Controller
     {
 
         try {
-            navigation::create([
-                'nav_id'        => Str::uuid(),
-                'id_label'      => $request->label,
-                'url'           => $request->url,
-                'navigation'    => $request->name,
-                'icon'          => $request->icon,
-                'is_active'     => 1
-            ]);
+            $this->navigationImplement->create($request);
 
             $notif = array(
                 'message' => 'Success Create Navigation',
@@ -49,9 +50,9 @@ class NavigationController extends Controller
     public function update(NavigationRequest $request)
     {
         try {
-            $navigation = navigation::where('nav_id', $request->id);
+            $navigation = $this->navigationImplement->getId($request->id);
 
-            if (!$navigation->get()) {
+            if (!$navigation) {
                 $notif = array(
                     'message' => 'Update Navigation Failed',
                     'alert-info' => 'warning'
@@ -60,12 +61,7 @@ class NavigationController extends Controller
                 return redirect()->back()->with($notif);
             };
 
-            $navigation->update([
-                'id_label'      => $request->label,
-                'url'           => $request->url,
-                'navigation'    => $request->name,
-                'icon'          => $request->icon,
-            ]);
+            $this->navigationImplement->update($request->id, $request);
 
             $notif = array(
                 'message' => 'Success Create Navigation',
@@ -86,9 +82,9 @@ class NavigationController extends Controller
     public function changeStatus($id)
     {
         try {
-            $navigation = navigation::where('nav_id', $id);
+            $navigation = $this->navigationImplement->getId($id);
 
-            if (!$navigation->get()) {
+            if (!$navigation) {
                 $notif = array(
                     'message' => 'Update Navigation Failed',
                     'alert-info' => 'warning'
@@ -98,12 +94,7 @@ class NavigationController extends Controller
             };
 
 
-            $check = $navigation->first();
-
-
-            $navigation->update([
-                'is_active' => ($check->is_active == 0) ? 1 : 0,
-            ]);
+            $this->navigationImplement->updateStatus($id, $navigation);
 
             $notif = array(
                 'message' => 'Success Change Status Navigation',
@@ -124,9 +115,9 @@ class NavigationController extends Controller
     public function destroy(Request $request)
     {
         try {
-            $navigation = navigation::where('nav_id', $request->id);
+            $navigation = $this->navigationImplement->getId($request->id);
 
-            if (!$navigation->get()) {
+            if (!$navigation) {
                 $notif = array(
                     'message' => 'Navigation Not Failed',
                     'alert-info' => 'warning'
@@ -135,7 +126,7 @@ class NavigationController extends Controller
                 return redirect()->back()->with($notif);
             };
 
-            $navigation->delete();
+            $this->navigationImplement->delete($request->id);
 
             $notif = array(
                 'message' => 'Success Delete Navigation',
